f65d7c454a16fbcc462dbe519ba7aecce1e9b387
Matheus Castro <mpcastro0525@gmail.com>
2024-03-16 19:16:35 -0400

    Upload2

--- NODEAPI/server.js
+++ NODEAPI/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("NODE API");
-    req
+  res.send("NODE API");
+  req;
+});
+app.get("/welcome", (req, res) => {
+  res.send("Welcome to my node training");
+  req;
 });
 
-app.listen(3000, () => {
-    console.log("Node API running on port 3000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@nodeapi.vxkfmqp.mongodb.net/API-test?retryWrites=true&w=majority&appName=NodeAPI"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Node API running on port 3000");
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
