0e3eef370a574cd7efff94945d949c138421a167
Matheus Castro <mpcastro0525@gmail.com>
2024-03-17 16:13:06 -0400

    crud completed

--- NODEAPI/server.js
+++ NODEAPI/server.js
@@ -7,9 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(
   express.urlencoded({
-    type: "*/x-www-form-urlencoded ",
     extended: true,
-  }) //NAO TA LENDO ESSA BUCETA DE REQUEST EM FORM DATA
+  })
 );
 
 app.get("/", (req, res) => {
@@ -62,7 +61,22 @@ app.put("/customer/:id", async (req, res) => {
       return res.status(404).json({ message: "Customer not found" });
     }
     const customerUpdated = await CustomerModel.findById(id);
+    console.log("Customer update. New values", customerUpdated);
     res.status(200).json(customerUpdated);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//DELETE
+app.delete("/customer/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const customer = await CustomerModel.findByIdAndDelete(id);
+    if (!customer) {
+      return res.status(404).json({ message: "Customer not found" });
+    }
+    res.status(200).json(customer);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
